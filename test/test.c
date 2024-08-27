8cd562d923112936473f94bed3c90ef896f70ba7
jf-home <Orig5826@163.com>
2024-08-27 21:16:21 +0800

    把测试框架搭建起来，后续添加测试内容。

---
+++ test/test.c
@@ -0,0 +1,17 @@
+/**
+ * @file test.c
+ * @author wenjf (Orig5826@163.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "test.h"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
---
+++ test/test.h
@@ -0,0 +1,21 @@
+/**
+ * @file test.h
+ * @author wenjf (Orig5826@163.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-27
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "unicstl.h"
+#include "unity.h"
+
+#endif
